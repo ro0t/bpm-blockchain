33077789f71ef606063b416753e5932dc9ca9ffd
Adam <adam@igital.co>
2019-07-03 20:52:29 +0000

    Remove .idea and add tcp serverrrr

---
+++ tcp-server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+)
+
+var bcServer chan []Block
+
+func main() {
+	err := godotenv.Load()
+
+}
